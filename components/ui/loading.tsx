3df9de64a66a6c7a2f652f9420064e45e91e4796
Ratnesh Kumar Sharma <rksiitd123@gmail.com>
2025-09-07 12:56:40 +0530

    fix: Build Issue

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -4,10 +4,3 @@ export { DonationLoading } from "./donation-loading"
 export { PageLoading } from "./page-loading"
 export { Skeleton, SkeletonCard, SkeletonForm, SkeletonGrid } from "./skeleton-loading"
 export { LoadingState } from "./loading-states"
-
-// Convenience exports for common use cases
-export { LoadingState as Loading } from "./loading-states"
-export { LoadingState as SectionLoading } from "./loading-states"
-export { LoadingState as CardLoading } from "./loading-states"
-export { LoadingState as FormLoading } from "./loading-states"
-export { LoadingState as PaymentLoading } from "./loading-states"
